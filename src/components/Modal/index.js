a5a3396be8ac57cf9b380d358430ea9b5ad9e788
illas <const.smartsolutions@gmail.com>
2025-05-21 22:36:39 +0900

    test

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,7 +1,7 @@
 import "./style.scss";
 import React from "react";
 
-export const Modal = ({ open, onClose, children }) => {
+export const Modal = ({ open, onClose, children, zindex, offset = 0 }) => {
   if (!open) return (
     <div>
       <h1>Not Found Modals</h1>
@@ -9,8 +9,8 @@ export const Modal = ({ open, onClose, children }) => {
   );
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal" onClick={(e) => e.stopPropagation(e)}>
+    <div className="modal-overlay" onClick={onClose} style={{ zIndex: zindex }}>
+      <div className="modal" onClick={(e) => e.stopPropagation(e)} style={{ transform: `translateY(${offset}px)` }}>
         <button className="close-button" onClick={() => onClose()}>X</button>
         { children }
       </div>
