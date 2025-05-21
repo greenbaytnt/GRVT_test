9c5d2c2e61f938002a905c2795b2ed5570867551
coinhades <sn102a1k46@gmail.com>
2025-05-21 16:53:48 +0700

    Nam Nguyen: Test_Project dynamic modals open

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import { Modal } from "./components/Modal";
+import React from "react";
 
 export default function App() {
+  const [modals, setModals] = useState([]);
+
+  const openModal = () => {
+    const id = Date.now();
+    setModals((prev) => [...prev, id]);
+  }
+
+  const closeModal = (id) => {
+    setModals((prev) => prev.filter((modalId) => modalId !== id));
+  }
+
   return (
     <div>
-      <div>
-        <button>Open Modal</button>
-      </div>
+      <h1>Hello World!</h1>
+      <button onClick={openModal}>Open Modal</button>
 
-      <Modal></Modal>
+      {modals.map((id, index) => (
+        <Modal
+          key={id}
+          open={true}
+          onClose={() => closeModal(id)}
+        >
+          <div>
+            <h2>Modal #{index + 1}</h2>
+            <p>Example Modal</p>
+          </div>
+        </Modal>
+      ))}
     </div>
   );
 }
+
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,5 +1,19 @@
 import "./style.scss";
+import React from "react";
 
-export const Modal = () => {
-  return <div className="modal">Modal Component</div>;
+export const Modal = ({ open, onClose, children }) => {
+  if (!open) return (
+    <div>
+      <h1>Not Found Modals</h1>
+    </div>
+  );
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal" onClick={(e) => e.stopPropagation(e)}>
+        <button className="close-button" onClick={() => onClose()}>X</button>
+        { children }
+      </div>
+    </div>
+  );
 };
